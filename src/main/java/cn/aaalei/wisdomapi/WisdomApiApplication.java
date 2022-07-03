2486a5556e574a842c0be9b27aa439c45c872263
WeiShao <1316833858@qq.com>
2022-07-03 14:17:50 +0800

    :tada: Initialize Spring Boot

---
+++ src/main/java/cn/aaalei/wisdomapi/WisdomApiApplication.java
@@ -0,0 +1,13 @@
+package cn.aaalei.wisdomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisdomApiApplication.class, args);
+	}
+
+}
